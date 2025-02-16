2f0302f4623ce21fbe8a700705b2f59a8647cb70
juandsw <juandavidgallegohernandez@gmail.com>
2025-02-15 20:05:19 -0500

    Primer commit. Linea base

---
+++ src/main/java/com/floristeria/floristeria_app/FloristeriaAppApplication.java
@@ -0,0 +1,13 @@
+package com.floristeria.floristeria_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloristeriaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloristeriaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/floristeria/floristeria_app/FloristeriaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floristeria.floristeria_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloristeriaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
